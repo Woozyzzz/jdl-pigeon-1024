c7bde401b6d40abbbae7a5802d12558a809280f9
Woozyzzz <woozyzzz@foxmail.com>
2023-03-01 14:51:19 +0800

    refactor(main): 重构动画函数方法

--- src/main.js
+++ src/main.js
@@ -1,53 +1,59 @@
-console.log("hi");
-let html = document.querySelector("p");
-let style = document.querySelector("#style");
-let string = `/* 大家好! 👋
+const el = (selector, element = document) => element.querySelectorAll(selector);
+
+const [elParagraph] = el(".paragraph");
+const [elStyle] = el("#style");
+
+const sourceText = `/* 大家好! 👋
  * 我是京东物流-技术发展部-中台技术部-技术平台组的JD star 🚀
  * 武林秀
  * 接下来我给大家画一个快递小鸽
  * 首先准备一个背景 */
-figure {
+.figure {
+  position: relative;
   height: 50vh;
   background: #62656c;
-  position: relative;
 }
 /* 画一个圈圈 🌑 */
 .head {
+  position: absolute;
+  top: 50%;
+  left: 50%;
   width: 100px;
   height: 100px;
   background: #7c8499;
   border: 4px solid #000;
   border-radius: 50%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
   transform: translate(-50%, -50%);
 }
 /* 给小鸽画个帽子 🧢 */
 .capWrapper {
+  position: absolute;
+  top: -5%;
+  left: 50%;
   width: 45px;
   height: 25px;
   background: #e23232;
   border: 4px solid #000;
   border-radius: 30px 30px 10px 10px;
-  position: absolute;
-  top: -5%;
-  left: 50%;
   transform: translate(-50%, -50%);
+
 }
 /* 帽子上有个尖尖 ✐ */
 .capU {
+  position: absolute;
+  top: -36%;
+  left: 50%;
   width: 3px;
   height: 6px;
   background: #000;
   border-radius: 50%;
-  position: absolute;
-  top: -36%;
-  left: 50%;
   transform: translateX(-50%);
 }
 /* 左边画一瓣 👈 */
 .capL {
+  position: absolute;
+  bottom: -5%;
+  left: 20%;
   width: 40px;
   height: 22px;
   background: transparent;
@@ -56,12 +62,12 @@ figure {
   border-right: none;
   border-bottom: none;
   border-radius: 20px 0 0 0;
-  position: absolute;
-  bottom: -5%;
-  left: 20%;
 }
 /* 右边画一瓣 👉 */
 .capR {
+  position: absolute;
+  bottom: -5%;
+  right: 20%;
   width: 40px;
   height: 22px;
   background: transparent;
@@ -70,88 +76,89 @@ figure {
   border-left: none;
   border-bottom: none;
   border-radius: 0 20px 0 0;
-  position: absolute;
-  bottom: -5%;
-  right: 20%;
 }
 /* 还有一个帽檐儿 🤠*/
 .capD {
+  position: absolute;
+  bottom: -20%;
+  left: -40%;
   width: 15px;
   height: 5px;
   background: #000;
   border-radius: 50% 0 0 50%;
-  position: absolute;
-  bottom: -20%;
-  left: -40%;
   transform: rotate(-5deg);
 }
 /* 接着画小鸽的眼睛 👀 */
 .eyeL {
+  position: absolute;
+  top: 30%;
+  left: 20%;
   width: 8px;
   height: 8px;
   background: #000;
   border-radius: 50%;
-  position: absolute;
-  top: 30%;
-  left: 20%;
 }
 /* 眯起来 😄 */
 .eyeR1 {
-  width: 10px;
-  height: 3px;
-  background: #000;
-  border-radius: 4px;
   position: absolute;
   top: 31%;
   left: 38%;
-  transform: rotate(-30deg);
-}
-.eyeR2 {
-  content: "";
-  display: block;
   width: 10px;
   height: 3px;
   background: #000;
   border-radius: 4px;
+  transform: rotate(-30deg);
+}
+.eyeR2 {
   position: absolute;
   top: 34.5%;
   left: 38%;
+  display: block;
+  width: 10px;
+  height: 3px;
+  content: "";
+  background: #000;
+  border-radius: 4px;
   transform: rotate(20deg);
 }
 /* 小小的眉毛 😊 */
 .eyeLup {
-  width: 7px;
-  height: 3px;
-  background: #000;
-  border-radius: 4px;
   position: absolute;
   top: 23%;
   left: 23%;
-  transform: rotate(30deg);
-}
-.eyeRup {
   width: 7px;
   height: 3px;
   background: #000;
   border-radius: 4px;
+  transform: rotate(30deg);
+}
+.eyeRup {
   position: absolute;
   top: 23%;
   left: 38%;
+  width: 7px;
+  height: 3px;
+  background: #000;
+  border-radius: 4px;
   transform: rotate(-30deg);
 }
 /* 然后画小鸽的嘴巴 👄 */
 .mouthU {
+  position: absolute;
+  top: 40%;
+  left: 18%;
   width: 30px;
   height: 10px;
   background: #ffa2a6;
   border: 4px solid #000;
   border-radius: 50%/100% 100% 0 0;
   border-bottom: none;
-  position: absolute;
-  top: 40%;
-  left: 18%;
 }
 .mouthM {
+  position: absolute;
+  top: 37%;
+  left: 19.5%;
+  z-index: 1;
   width: 27px;
   height: 20px;
   background: transparent;
@@ -160,86 +167,85 @@ figure {
   border-top: none;
   border-left: none;
   border-right: none;
-  position: absolute;
-  top: 37%;
-  left: 19.5%;
-  z-index: 1;
 }
 .mouthD {
+  position: absolute;
+  top: 50%;
+  left: 18%;
+  z-index: -1;
   width: 30px;
   height: 20px;
   background: #ffa2a6;
   border: 4px solid #000;
   border-radius: 50%/0 0 100% 100%;
   border-top: none;
-  position: absolute;
-  top: 50%;
-  left: 18%;
-  z-index: -1;
 }
 /* 点两个鼻孔 👃 */
 .mouthM::before {
-  content: "";
-  display: block;
-  width: 3px;
-  height: 3px;
-  background: #000;
-  border-radius: 50%;
   position: absolute;
   top: 50%;
   left: 35%;
-}
-.mouthM::after {
-  content: "";
   display: block;
   width: 3px;
   height: 3px;
+  content: "";
   background: #000;
   border-radius: 50%;
+}
+.mouthM::after {
   position: absolute;
   top: 50%;
   left: 55%;
+  display: block;
+  width: 3px;
+  height: 3px;
+  content: "";
+  background: #000;
+  border-radius: 50%;
 }
 /* 优雅又不失俏皮的舌头 🤪 */
 .tough {
+  position: absolute;
+  bottom: -5%;
+  right: 10%;
   width: 8px;
   height: 6px;
   background: #e23232;
   border: 2px solid #000;
   border-radius: 80% 50% /100% 100% 0 0;
-  position: absolute;
-  bottom: -5%;
-  right: 10%;
 }
 .tough::before {
-  content: "";
-  display: block;
-  width: 2px;
-  height: 4px;
-  background: #000;
-  border-radius: 50%;
   position: absolute;
   top: 50%;
   left: 40%;
+  display: block;
+  width: 2px;
+  height: 4px;
+  content: "";
+  background: #000;
+  border-radius: 50%;
 }
 /* 么么哒 💖*/
 .heartWrapper{
-  /* border: 1px solid #000; */
   position: absolute;
   top: 30%;
   right: 20%;
   transform: rotate(20deg);
+  /* border: 1px solid #000; */
 }
 .heart {
   width: 8px;
   height: 8px;
   background: #ffa2a6;
   border: 2px solid #000;
-  transform: rotate(-45deg);
   border-top: none;
   border-right: none;
+  transform: rotate(-45deg);
 }
 .heartL {
+  position: absolute;
+  top: -50%;
+  left: -45%;
   width: 8px;
   height: 9px;
   background: #ffa2a6;
@@ -247,21 +253,20 @@ figure {
   border-radius: 50% 50% 0 0;
   border-bottom: 2px solid #ffa2a6;
   transform: rotate(-45deg);
-  position: absolute;
-  top: -50%;
-  left: -45%;
 }
 .heartM {
-  width: 2px;
-  height: 2px;
-  background: #000;
-  transform: rotate(-45deg);
   position: absolute;
   top: -18%;
   left: 50%;
+  width: 2px;
+  height: 2px;
+  background: #000;
   transform: translate(-50%) rotate(45deg);
 }
 .heartR {
+  position: absolute;
+  top: -50%;
+  right: -45%;
   width: 8px;
   height: 9px;
   background: #ffa2a6;
@@ -269,39 +274,41 @@ figure {
   border-radius: 50% 50% 0 0;
   border-bottom: 2px solid #ffa2a6;
   transform: rotate(45deg);
-  position: absolute;
-  top: -50%;
-  right: -45%;
 }
 /* 一幅快递小鸽送给大家~
  * 我是京东物流-技术发展部-中台技术部-技术平台组的
  * 武林秀
  * 以梦为码，不负韶华
  * 祝大家1024快乐 👍 */
-
-
  `;
-let string2 = "";
-let n = 0;
 
-let step = () => {
+let paragraphText = "";
+let index = 0;
+
+const step = () => {
   setTimeout(() => {
-    if (n < string.length) {
-      if (string[n] === "\n") {
-        string2 += "<br>";
-      } else if (string[n] === " ") {
-        string2 += "&nbsp;";
+    if (index < sourceText.length) {
+      if (sourceText[index] === "\n") {
+        paragraphText += "<br>";
+      } else if (sourceText[index] === " ") {
+        paragraphText += "&nbsp;";
       } else {
-        string2 += string[n];
+        paragraphText += sourceText[index];
       }
-      html.innerHTML = string2;
-      window.scrollTo(0, 9999);
-      html.scrollTo(0, 9999);
-      style.innerHTML = string.slice(0, n);
-      n += 1;
+      elParagraph.innerHTML = paragraphText;
+      window.scroll({
+        top: 9999,
+        behavior: "smooth",
+      });
+      elParagraph.scroll({
+        top: 9999,
+        behavior: "smooth",
+      });
+      elStyle.innerHTML = sourceText.slice(0, index);
+      index += 1;
       step();
     }
-  }, 20);
+  }, 10);
 };
 
 step();
